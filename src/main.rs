a956969f3f2e2a0b1ce7468611e8e6730dccc2a6
Bitflora <>
2024-09-03 22:17:51 -0400

    some classes

--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,35 @@ const BOARD_HEIGHT: usize = 40;
 
 const SQUARE_SIZE: usize = 20;
 
+const WINDOW_WIDTH: usize = BOARD_WIDTH * SQUARE_SIZE;
+const WINDOW_HEIGHT: usize = BOARD_HEIGHT * SQUARE_SIZE;
+
+type Color = u32;
+
+struct Block {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Block {
+    const SIZE: usize = SQUARE_SIZE;
+}
+
 fn main() {
-    const WIDTH: usize = BOARD_WIDTH * SQUARE_SIZE;
-    const HEIGHT: usize = BOARD_HEIGHT * SQUARE_SIZE;
 
-    let mut board: Vec<u32> = vec![0; BOARD_WIDTH * BOARD_HEIGHT];
+    let mut board: Vec<Color> = vec![0; BOARD_WIDTH * BOARD_HEIGHT];
+    let mut buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
+    let mut window = init_window();
 
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-    
-
-    let mut window = Window::new(
-        "TetRust - ESC to exit",
-        WIDTH,
-        HEIGHT,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("Window creation failed: {}", e);
-    });
-
-    set_block(&mut board, 3, 7, 0xff0000);
-    set_block(&mut board, 5, 5, 0x00ff00);
+    set_block(&mut board, Block{ x: 3, y: 7}, 0xff0000);
+    set_block(&mut board, Block{ x: 5, y: 5}, 0x00ff00);
+    set_block(&mut board, Block{ x: 5, y: 6}, 0x0000ff);
 
     let mut running = true;
 
     while running {
         render_board(&board, &mut buffer);
-        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+        window.update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT).unwrap();
         sleep(time::Duration::from_millis(100));
 
         if window.is_key_down(Key::Escape) {
@@ -42,19 +44,33 @@ fn main() {
     }
 }
 
+fn init_window() -> Window {
+    let window = Window::new(
+        "TetRust - ESC to exit",
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("Window creation failed: {}", e);
+    });
+
+    window
+}
+
 fn render_board(board: &Vec<u32>, buffer: &mut Vec<u32>) {
     for x in 0..BOARD_WIDTH {
         for y in 0..BOARD_HEIGHT {
             let color = board[y*BOARD_WIDTH + x];
-            for ix in 0..SQUARE_SIZE {
-                for iy in 0..SQUARE_SIZE {
-                    buffer[(y*SQUARE_SIZE)*BOARD_WIDTH*SQUARE_SIZE + iy*BOARD_WIDTH*SQUARE_SIZE + x*SQUARE_SIZE + ix] = color;
+            for ix in 0..Block::SIZE {
+                for iy in 0..Block::SIZE {
+                    buffer[(y*Block::SIZE)*BOARD_WIDTH*Block::SIZE + iy*BOARD_WIDTH*Block::SIZE + x*Block::SIZE + ix] = color;
                 }
             }
         }
     }
 }
 
-fn set_block(board: &mut Vec<u32>, x: usize, y: usize, val: u32) {
-    board[y * BOARD_WIDTH + x] = val;
+fn set_block(board: &mut Vec<u32>, b: Block, val: Color) {
+    board[b.y * BOARD_WIDTH + b.x] = val;
 }
