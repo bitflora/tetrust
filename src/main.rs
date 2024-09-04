b1ec27b7f7e8dedf7045d929a9f0ed74d3ce0913
Bitflora <>
2024-09-03 22:50:55 -0400

    A shape appears and can be moved left and right

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{thread::sleep, time};
+use std::{thread::{current, sleep}, time};
 
 use minifb::{Key, Window, WindowOptions};
 
@@ -12,6 +12,7 @@ const WINDOW_HEIGHT: usize = BOARD_HEIGHT * SQUARE_SIZE;
 
 type Color = u32;
 
+#[derive(Copy, Clone)]
 struct Block {
     pub x: usize,
     pub y: usize,
@@ -19,6 +20,63 @@ struct Block {
 
 impl Block {
     const SIZE: usize = SQUARE_SIZE;
+}
+
+struct Shape {
+    blocks: Vec<Block>,
+    color: Color,
+}
+
+impl Shape {
+    fn line(left: Block) -> Shape {
+        return Shape {
+            blocks: vec![
+                left.clone(),
+                Block{ x: left.x + 1, y: left.y },
+                Block{ x: left.x + 2, y: left.y },
+                Block{ x: left.x + 3, y: left.y },
+            ],
+            color: 0x770077
+        };
+    }
+
+    fn square(upperleft: Block) -> Shape {
+        return Shape {
+            blocks: vec![
+                upperleft.clone(),
+                Block{ x: upperleft.x + 1, y: upperleft.y },
+                Block{ x: upperleft.x, y: upperleft.y + 1 },
+                Block{ x: upperleft.x + 1, y: upperleft.y + 1 },
+            ],
+            color: 0x770077
+        };
+    }
+
+    fn move_right(&mut self) -> bool {
+        for block in &self.blocks {
+            if block.x + 1 >= BOARD_WIDTH {
+                return false;
+            }
+        }
+
+        for block in &mut self.blocks {
+            block.x += 1;
+        }
+        return true;
+    }
+
+    fn move_left(&mut self) -> bool {
+        for block in &self.blocks {
+            if block.x == 0 {
+                return false;
+            }
+        }
+
+        for block in &mut self.blocks {
+            block.x -= 1;
+        }
+        return true;
+    }
 }
 
 fn main() {
@@ -33,13 +91,19 @@ fn main() {
 
     let mut running = true;
 
+    let mut curr_shape: Shape = Shape::square(Block{x: 0, y: 0});
+
     while running {
-        render_board(&board, &mut buffer);
+        render_board(&board, &mut buffer, &curr_shape);
         window.update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT).unwrap();
         sleep(time::Duration::from_millis(100));
 
         if window.is_key_down(Key::Escape) {
             running = false;
+        } else if window.is_key_down(Key::Right) {
+            curr_shape.move_right();
+        } else if window.is_key_down(Key::Left) {
+            curr_shape.move_left();
         }
     }
 }
@@ -58,16 +122,24 @@ fn init_window() -> Window {
     window
 }
 
-fn render_board(board: &Vec<u32>, buffer: &mut Vec<u32>) {
+fn render_board(board: &Vec<u32>, buffer: &mut Vec<u32>, curr_shape: &Shape) {
+    let mut draw_block = |x: usize, y: usize, color: Color| {
+        for ix in 0..Block::SIZE {
+            for iy in 0..Block::SIZE {
+                buffer[(y*Block::SIZE)*BOARD_WIDTH*Block::SIZE + iy*BOARD_WIDTH*Block::SIZE + x*Block::SIZE + ix] = color;
+            }
+        }
+    };
+
     for x in 0..BOARD_WIDTH {
         for y in 0..BOARD_HEIGHT {
             let color = board[y*BOARD_WIDTH + x];
-            for ix in 0..Block::SIZE {
-                for iy in 0..Block::SIZE {
-                    buffer[(y*Block::SIZE)*BOARD_WIDTH*Block::SIZE + iy*BOARD_WIDTH*Block::SIZE + x*Block::SIZE + ix] = color;
-                }
-            }
+            draw_block(x, y, color);
         }
+    }
+
+    for block in &curr_shape.blocks {
+        draw_block(block.x, block.y, curr_shape.color);
     }
 }
 
